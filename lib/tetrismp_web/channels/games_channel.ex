5aab61d9651998e58613188508f6fcd2c98e93de
Noah <nakennedy11@gmail.com>
2019-02-21 17:36:34 -0500

    todo: broadcasting

--- lib/tetrismp_web/channels/games_channel.ex
+++ lib/tetrismp_web/channels/games_channel.ex
@@ -8,7 +8,7 @@ defmodule TetrismpWeb.GamesChannel do
   intercept ["update"]
 
   def join("games:"<>name,  payload, socket) do
-    
+
     if authorized?(payload) do
       game = BackupAgent.get(name) || Game.new()
       BackupAgent.put(name, game)
@@ -16,10 +16,11 @@ defmodule TetrismpWeb.GamesChannel do
       |> assign(:game, game)
       |> assign(:name, name)
       |> assign(:user, payload)
-      GameServer.start(name)
+      GameServer.start(name, payload)
       IO.puts(socket.assigns[:user])
-
+      IO.inspect(socket)
       {:ok, %{"joined" => name, "game" => game}, socket}
+
 
     else
       {:error, %{reason: "unauthorized"}}
@@ -30,38 +31,38 @@ defmodule TetrismpWeb.GamesChannel do
   # by sending replies to requests from the client
   def handle_in("render_piece", %{}, socket) do
     game = GameServer.render_piece(socket.assigns[:name])
-    push_update! game, socket
+    #push_update! game, socket
     {:reply, {:ok, %{"game" => game}}, socket} 
   end
 
   def handle_in("render_next_piece", %{}, socket) do
     game = GameServer.render_next_piece(socket.assigns[:name])
-    push_update! game, socket
+    #push_update! game, socket
     {:reply, {:ok, %{"game" => game}}, socket}
   end
 
   def handle_in("piece_fall", %{}, socket) do
     game = GameServer.piece_fall(socket.assigns[:name])
-    push_update! game, socket
+    #push_update! game, socket
     {:reply, {:ok, %{"game" => game}}, socket}
   end
 
   def handle_in("rotate", %{}, socket) do
     game = GameServer.change_orientation(socket.assigns[:name])
-    push_update! game, socket
+    #push_update! game, socket
     {:reply, {:ok, %{"game" => game}}, socket}
  
   end
 
   def handle_in("move", %{"direction" => direction}, socket) do
     game = GameServer.move(socket.assigns[:name], direction)
-    push_update! game, socket
+    #push_update! game, socket
     {:reply, {:ok, %{"game" => game}}, socket}
   end
 
   def handle_in("update_board", %{"board" => board}, socket) do
     game = GameServer.update_board(socket.assigns[:name], board)
-    push_update! game, socket
+    #push_update! game, socket
     {:reply, {:ok, %{"game" => game}}, socket}
   end
 
